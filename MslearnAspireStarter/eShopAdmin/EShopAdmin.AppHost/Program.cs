972157232bc6f1baefe239a9d1d44b5e64842c5c
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-09-10 16:59:57 -0500

    Add the orchestation reference to Store project

---
+++ MslearnAspireStarter/eShopAdmin/EShopAdmin.AppHost/Program.cs
@@ -0,0 +1,8 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.Store>("store");
+
+builder.AddProject<Projects.Products>("products")
+       .WithHttpEndpoint(port: 5200, name: "products");
+
+builder.Build().Run();
