0d63b7dfea2bc7b13b956fce5278b3ceb38a7749
Mario Andres Botero Florez <mariobot113@hotmail.com>
2024-09-11 18:36:47 -0500

    Update folder

--- MslearhAspireDatabase/mslearn-aspire-starter/eShopAdmin/finished-files/exampleApp.AppHost/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var products = builder.AddProject<Projects.Products>("products");
-
-builder.AddProject<Projects.Store>("store")
-  .WithReference(products);
-
-builder.Build().Run();
